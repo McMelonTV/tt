0ce83c33e37da85e0482673b9cadc14486501cf5
mel <mcmelon@nodiumhosting.com>
2025-09-17 11:59:25 +0200

    feat: basic file loading and editing (no saving yet)

--- internal/screen/edit_screen/edit_screen.go
+++ internal/screen/edit_screen/edit_screen.go
@@ -1,1 +1,161 @@
-package screen
+package edit_screen
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+	"tinytext/pkg/types"
+	"tinytext/pkg/util/math"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type EditScreen struct {
+	fileType *types.FileType
+	filePath *string
+
+	fileContent []string
+
+	cursorX uint
+	cursorY uint
+}
+
+func Create(filePath string) EditScreen {
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			_, err := os.Create(filePath)
+			if err != nil {
+				panic(err)
+			}
+			file, err = os.ReadFile(filePath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+
+	fileStr := string(file)
+
+	fileContent := strings.Split(fileStr, "\n")
+
+	return EditScreen{
+		filePath: &filePath,
+
+		fileContent: fileContent,
+
+		cursorX: 0,
+		cursorY: 0,
+	}
+}
+
+func (m EditScreen) Init() tea.Cmd {
+	return nil
+}
+
+func (m EditScreen) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "up":
+			if m.cursorY > 0 {
+				m.cursorY--
+				lineWidth := uint(len(m.fileContent[m.cursorY]))
+				m.cursorX = math.ClampUintMinMax(m.cursorX, 0, lineWidth)
+			}
+		case "down":
+			if m.cursorY < uint(len(m.fileContent)-1) {
+				m.cursorY++
+				lineWidth := uint(len(m.fileContent[m.cursorY]))
+				m.cursorX = math.ClampUintMinMax(m.cursorX, 0, lineWidth)
+			}
+		case "left":
+			if m.cursorX > 0 {
+				m.cursorX--
+			} else {
+				if m.cursorY > 0 {
+					m.cursorY--
+					lineWidth := uint(len(m.fileContent[m.cursorY]))
+					m.cursorX = lineWidth
+				}
+			}
+		case "right":
+			if m.cursorX < uint(len(m.fileContent[m.cursorY])) {
+				m.cursorX++
+			} else {
+				if m.cursorY < uint(len(m.fileContent)-1) {
+					m.cursorY++
+					m.cursorX = 0
+				}
+			}
+		case "tab":
+			line := m.fileContent[m.cursorY]
+			before, after := line[:m.cursorX], line[m.cursorX:]
+			m.fileContent[m.cursorY] = fmt.Sprintf("%s%s%s", before, "    ", after)
+			m.cursorX += uint(len("    "))
+		case "backspace":
+			if m.cursorX != 0 {
+				line := m.fileContent[m.cursorY]
+				before, after := line[:m.cursorX-1], line[m.cursorX:]
+				m.fileContent[m.cursorY] = fmt.Sprintf("%s%s", before, after)
+				m.cursorX -= 1
+			} else {
+				if m.cursorY > 0 {
+					lineBefore, line := m.fileContent[m.cursorY-1], m.fileContent[m.cursorY]
+					m.fileContent[m.cursorY-1] = fmt.Sprintf("%s%s", lineBefore, line)
+					m.fileContent = slices.Delete(m.fileContent, int(m.cursorY), int(m.cursorY+1))
+					m.cursorX = uint(len(lineBefore))
+					m.cursorY -= 1
+				}
+			}
+		case "enter":
+			line := m.fileContent[m.cursorY]
+			before, after := line[:m.cursorX], line[m.cursorX:]
+			m.fileContent[m.cursorY] = fmt.Sprintf("%s", before)
+			m.fileContent = slices.Insert(m.fileContent, int(m.cursorY+1), fmt.Sprintf("%s", after))
+			m.cursorY += 1
+			m.cursorX = 0
+		default:
+			line := m.fileContent[m.cursorY]
+			before, after := line[:m.cursorX], line[m.cursorX:]
+			m.fileContent[m.cursorY] = fmt.Sprintf("%s%s%s", before, string(msg.Runes), after)
+			m.cursorX += uint(len(string(msg.Runes)))
+		}
+	}
+
+	return m, nil
+}
+
+func (m EditScreen) View() string {
+	s := "TinyText"
+	s += "\n===============\n"
+
+	for y, l := range m.fileContent {
+		var line string
+
+		if m.cursorY == uint(y) {
+			isFinal := m.cursorX == uint(len(l))
+			var before, char, after string
+
+			if !isFinal {
+				before, char, after = l[:m.cursorX], l[m.cursorX:m.cursorX+1], l[m.cursorX+1:]
+			} else {
+				before, char, after = l, " ", ""
+			}
+
+			line = fmt.Sprintf("%s\u001B[47;30m%s\u001B[0m%s", before, char, after)
+		} else {
+			line = l
+		}
+
+		s += fmt.Sprintf("%s\n", line)
+	}
+
+	return s
+}
--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-  "fmt"
+	"tinytext/internal/screen/edit_screen"
+
+	"github.com/alexflint/go-arg"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+var Args struct {
+	FilePath string `arg:"positional,required"`
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
+	arg.MustParse(&Args)
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	screen := tea.NewProgram(edit_screen.Create(Args.FilePath))
+	if _, err := screen.Run(); err != nil {
+		panic(err)
+	}
 }
